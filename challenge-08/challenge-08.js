6f695422b6c2cc72a7a0d9d081fe8c79bd5115b9
Marcelo Antônio Chaves Mendes <75353400+maeceloacm1998@users.noreply.github.com>
2020-12-23 05:20:22 -0300

    Update challenge-08.js

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,38 +3,42 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+var sum = function calculateSum (x,y){
+  retunr x + y;
+}
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+console.log(' A soma de 5 e 5 é igual a'+ sun(5,5));
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+console.log('O nome da função que faz a soma é '+ sum.name); 
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+function showName (){
+  return 'Marcelo';
+}
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
+var VerShowName = showName();
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+console.log('A função VerShowName retorna '+ VerShowName);
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -48,7 +52,12 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-// ?
+function calculator (operador){
+  return function (num1,num2){
+      return num1 + operador + num2;
+  }
+  
+}
 
 /*
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
